1db61865a59776bc67eea106809a62682b7a3007
Chiara Pinto <pintochiara@itis-molinari.eu>
2024-10-15 11:52:38 +0200

    tabella

---
+++ albergo_c.js
@@ -0,0 +1,16 @@
+const table = document.getElementById("tabella");
+const template =  `
+<tr>
+    <td>
+        %DATA
+    </td>
+    <td>
+        %SINGOLA
+    </td>
+    <td>
+        %DOPPIA
+    </td>
+    <td>
+        %SUITE
+    </td>
+</tr>\n`;
